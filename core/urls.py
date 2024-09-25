d3a28741e14a8dcf7eeca57f32e46be299079dc3
kabrownie <makonokevin@gmail.com>
2024-09-25 17:23:26 +0300

    form

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,8 @@ from . import views
 app_name ='core'
 
 urlpatterns = [
+    path('',views.index, name='index'),
+   path('contact/', views.contact, name='contact'),
+   path('signup/', views.signup, name='signup'),
+   
 ]
