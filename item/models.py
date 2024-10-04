b9a357a600111c5805e04e31d509b5a63e75d3e9
kabrownie <makonokevin@gmail.com>
2024-10-04 20:03:46 +0300

    added delete dashboard

--- item/models.py
+++ item/models.py
@@ -15,12 +15,12 @@ class Category (models.Model):
 class Item (models.Model):
     name = models.CharField(max_length=255)
     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField()
     price = models.FloatField()
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    image = models.ImageField(upload_to='item_images',)
     
    
     ordering = ('name',)
--- soko/settings.py
+++ soko/settings.py
@@ -41,8 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #my apps
+    
     'core',
     'item',
+    'dashboard',
      # other apps
     'django.contrib.humanize',
 ]
